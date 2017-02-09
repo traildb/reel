47dd16810e93fa17899b4616af7186f5032c302e
Ville Tuulos <ville.tuulos@adroll.com>
2017-02-08 22:30:04 -0800

    add missing thread_util

---
+++ thread_util.h
@@ -0,0 +1,39 @@
+
+#ifndef TDBCLI_THREAD_UTIL
+#define TDBCLI_THREAD_UTIL
+
+#include <pthread.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DIE(msg, ...)\
+    do { fprintf(stderr, msg"\n", ##__VA_ARGS__);   \
+         exit(EXIT_FAILURE); } while (0)
+
+struct thread_job{
+    void *arg;
+    pthread_t thread;
+    int done;
+    int started;
+    int fresh;
+};
+
+typedef void *(*map_fun_t)(void*);
+typedef void *(*reduce_fun_t)(struct thread_job *jobs,
+                              uint32_t num_jobs,
+                              void *reduce_ctx);
+
+void execute_jobs(map_fun_t thread_fun,
+                  struct thread_job *jobs,
+                  uint32_t num_jobs,
+                  uint32_t num_threads);
+
+void *execute_jobs_with_reduce(map_fun_t map_fun,
+                               reduce_fun_t reduce_fun,
+                               struct thread_job *jobs,
+                               void *reduce_ctx,
+                               uint32_t num_jobs,
+                               uint32_t num_threads);
+
+#endif /* TDBCLI_THREAD_UTIL */
